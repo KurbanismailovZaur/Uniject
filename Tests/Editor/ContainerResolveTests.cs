54ab6f2f8f54ccd6a63f7834bc1adb3edeb82020
Zaur Kurbanismailov <Kurbanismailov.92@gmail.com>
2026-06-10 00:35:18 +0300

    test: cover object name and transform binding options

--- Tests/Editor/ContainerResolveTests.cs
+++ Tests/Editor/ContainerResolveTests.cs
@@ -510,6 +510,175 @@ namespace Uniject.Tests
         }
 
         [Test]
+        public void Resolve_FromNewComponentOnNewGameObject_WithObjectName_RenamesGameObject()
+        {
+            var resolvedScript = default(Script);
+
+            try
+            {
+                var container = new Container();
+                container.Bind<Script>()
+                    .FromNewComponentOnNewGameObject()
+                    .WithObjectName("Player")
+                    .AsTransient();
+
+                resolvedScript = container.Resolve<Script>();
+
+                Assert.That(resolvedScript.gameObject.name, Is.EqualTo("Player"));
+            }
+            finally
+            {
+                if (resolvedScript != null)
+                    UnityEngine.Object.DestroyImmediate(resolvedScript.gameObject);
+            }
+        }
+
+        [Test]
+        public void Resolve_FromNewComponentOnNewGameObject_UnderTransform_SetsParent()
+        {
+            var parent = new GameObject("Parent").transform;
+            var resolvedScript = default(Script);
+
+            try
+            {
+                var container = new Container();
+                container.Bind<Script>()
+                    .FromNewComponentOnNewGameObject()
+                    .UnderTransform(parent)
+                    .AsTransient();
+
+                resolvedScript = container.Resolve<Script>();
+
+                Assert.That(resolvedScript.transform.parent, Is.SameAs(parent));
+            }
+            finally
+            {
+                if (resolvedScript != null)
+                    UnityEngine.Object.DestroyImmediate(resolvedScript.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(parent.gameObject);
+            }
+        }
+
+        [Test]
+        public void Resolve_FromNewComponentOnNewGameObject_WithObjectNameAndUnderTransform_AppliesBoth()
+        {
+            var parent = new GameObject("Parent").transform;
+            var resolvedScript = default(Script);
+
+            try
+            {
+                var container = new Container();
+                container.Bind<Script>()
+                    .FromNewComponentOnNewGameObject()
+                    .WithObjectName("Enemy")
+                    .UnderTransform(parent)
+                    .AsTransient();
+
+                resolvedScript = container.Resolve<Script>();
+
+                Assert.That(resolvedScript.gameObject.name, Is.EqualTo("Enemy"));
+                Assert.That(resolvedScript.transform.parent, Is.SameAs(parent));
+            }
+            finally
+            {
+                if (resolvedScript != null)
+                    UnityEngine.Object.DestroyImmediate(resolvedScript.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(parent.gameObject);
+            }
+        }
+
+        [Test]
+        public void Resolve_FromComponentInNewPrefab_WithObjectName_RenamesClonedGameObject()
+        {
+            var prefabScript = new GameObject("Prefab").AddComponent<Script>();
+            var resolvedScript = default(Script);
+
+            try
+            {
+                var container = new Container();
+                container.Bind<Script>()
+                    .FromComponentInNewPrefab(prefabScript)
+                    .WithObjectName("Clone")
+                    .AsTransient();
+
+                resolvedScript = container.Resolve<Script>();
+
+                Assert.That(resolvedScript.gameObject.name, Is.EqualTo("Clone"));
+                Assert.That(prefabScript.gameObject.name, Is.EqualTo("Prefab"));
+            }
+            finally
+            {
+                if (resolvedScript != null)
+                    UnityEngine.Object.DestroyImmediate(resolvedScript.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(prefabScript.gameObject);
+            }
+        }
+
+        [Test]
+        public void Resolve_FromNewComponentOnNewPrefab_UnderTransform_ParentsClonedPrefab()
+        {
+            var prefab = new GameObject("Prefab");
+            var parent = new GameObject("Parent").transform;
+            var resolvedScript = default(Script);
+
+            try
+            {
+                var container = new Container();
+                container.Bind<Script>()
+                    .FromNewComponentOnNewPrefab(prefab)
+                    .UnderTransform(parent)
+                    .AsTransient();
+
+                resolvedScript = container.Resolve<Script>();
+
+                Assert.That(resolvedScript.transform.parent, Is.SameAs(parent));
+                Assert.That(prefab.transform.parent, Is.Null);
+            }
+            finally
+            {
+                if (resolvedScript != null)
+                    UnityEngine.Object.DestroyImmediate(resolvedScript.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(parent.gameObject);
+                UnityEngine.Object.DestroyImmediate(prefab);
+            }
+        }
+
+        [Test]
+        public void Resolve_AsCached_WithObjectNameAndUnderTransform_ConfiguresInstanceOnlyOnce()
+        {
+            var parent = new GameObject("Parent").transform;
+            var first = default(Script);
+
+            try
+            {
+                var container = new Container();
+                container.Bind<Script>()
+                    .FromNewComponentOnNewGameObject()
+                    .WithObjectName("Cached")
+                    .UnderTransform(parent)
+                    .AsCached();
+
+                first = container.Resolve<Script>();
+                var second = container.Resolve<Script>();
+
+                Assert.That(second, Is.SameAs(first));
+                Assert.That(first.gameObject.name, Is.EqualTo("Cached"));
+                Assert.That(first.transform.parent, Is.SameAs(parent));
+            }
+            finally
+            {
+                if (first != null)
+                    UnityEngine.Object.DestroyImmediate(first.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(parent.gameObject);
+            }
+        }
+
+        [Test]
         public void Bind_FromResolve_WhenContractTypeEqualsConcreteType_ThrowsArgumentException()
         {
             var container = new Container();
