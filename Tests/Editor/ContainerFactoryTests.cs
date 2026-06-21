54b772730486b71b2082a55b5fe41d6faffc1f5e
Zaur Kurbanismailov <Kurbanismailov.92@gmail.com>
2026-06-21 11:09:00 +0300

    Added tests for parametrized factory

--- Tests/Editor/ContainerFactoryTests.cs
+++ Tests/Editor/ContainerFactoryTests.cs
@@ -262,6 +262,235 @@ namespace Uniject.Tests
         }
 
         [Test]
+        public void BindFactoryWithParameter_WhenFactoryWasBound_ResolvesFactoryByFactoryType()
+        {
+            var prefabScript = new GameObject("Prefab").AddComponent<Script>();
+            var result = default(Script);
+
+            try
+            {
+                var container = new Container();
+                container.BindFactory<GameObject, Script>().FromComponentInNewPrefab().AsTransient();
+
+                var factory = container.Resolve<Factory<GameObject, Script>>();
+                result = factory.Create(prefabScript.gameObject);
+
+                Assert.That(factory, Is.Not.Null);
+                Assert.That(factory is IFactory<GameObject, Script>, Is.True);
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result, Is.Not.SameAs(prefabScript));
+                Assert.That(result.gameObject, Is.Not.SameAs(prefabScript.gameObject));
+            }
+            finally
+            {
+                if (result != null)
+                    UnityEngine.Object.DestroyImmediate(result.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(prefabScript.gameObject);
+            }
+        }
+
+        [Test]
+        public void ResolveFactoryWithParameter_AsTransient_ReturnsDifferentFactories()
+        {
+            var container = new Container();
+            container.BindFactory<GameObject, Script>().FromComponentInNewPrefab().AsTransient();
+
+            var first = container.Resolve<Factory<GameObject, Script>>();
+            var second = container.Resolve<Factory<GameObject, Script>>();
+
+            Assert.That(first, Is.Not.SameAs(second));
+        }
+
+        [Test]
+        public void ResolveFactoryWithParameter_AsCached_ReturnsSameFactoryButCreatesTransientResults()
+        {
+            var prefabScript = new GameObject("Prefab").AddComponent<Script>();
+            var firstResult = default(Script);
+            var secondResult = default(Script);
+
+            try
+            {
+                var container = new Container();
+                container.BindFactory<GameObject, Script>().FromComponentInNewPrefab().AsCached();
+
+                var firstFactory = container.Resolve<Factory<GameObject, Script>>();
+                var secondFactory = container.Resolve<Factory<GameObject, Script>>();
+                firstResult = firstFactory.Create(prefabScript.gameObject);
+                secondResult = secondFactory.Create(prefabScript.gameObject);
+
+                Assert.That(secondFactory, Is.SameAs(firstFactory));
+                Assert.That(firstResult, Is.Not.SameAs(secondResult));
+                Assert.That(firstResult.gameObject, Is.Not.SameAs(secondResult.gameObject));
+            }
+            finally
+            {
+                if (firstResult != null)
+                    UnityEngine.Object.DestroyImmediate(firstResult.gameObject);
+
+                if (secondResult != null)
+                    UnityEngine.Object.DestroyImmediate(secondResult.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(prefabScript.gameObject);
+            }
+        }
+
+        [Test]
+        public void Create_WithParameterFactoryAndConcreteResultType_ReturnsConcreteResultAsContract()
+        {
+            var prefabScript = new GameObject("Prefab").AddComponent<ScriptImplementedIInterface>();
+            var result = default(IInterface);
+
+            try
+            {
+                var container = new Container();
+                container.BindFactory<GameObject, IInterface>()
+                    .To<ScriptImplementedIInterface>()
+                    .FromComponentInNewPrefab()
+                    .AsTransient();
+
+                var factory = container.Resolve<Factory<GameObject, IInterface>>();
+                result = factory.Create(prefabScript.gameObject);
+
+                Assert.That(result, Is.TypeOf<ScriptImplementedIInterface>());
+                Assert.That(result, Is.Not.SameAs(prefabScript));
+                Assert.That(((Component)result).gameObject, Is.Not.SameAs(prefabScript.gameObject));
+            }
+            finally
+            {
+                if (result is Component resultComponent)
+                    UnityEngine.Object.DestroyImmediate(resultComponent.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(prefabScript.gameObject);
+            }
+        }
+
+        [Test]
+        public void Create_WithParameterFactory_WhenPrefabParameterIsComponent_ReturnsComponentFromClonedPrefab()
+        {
+            var prefabScript = new GameObject("Prefab").AddComponent<Script>();
+            var result = default(Script);
+
+            try
+            {
+                var container = new Container();
+                container.BindFactory<Script, Script>().FromComponentInNewPrefab().AsTransient();
+
+                var factory = container.Resolve<Factory<Script, Script>>();
+                result = factory.Create(prefabScript);
+
+                Assert.That(result, Is.Not.Null);
+                Assert.That(result, Is.Not.SameAs(prefabScript));
+                Assert.That(result.gameObject, Is.Not.SameAs(prefabScript.gameObject));
+            }
+            finally
+            {
+                if (result != null)
+                    UnityEngine.Object.DestroyImmediate(result.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(prefabScript.gameObject);
+            }
+        }
+
+        [Test]
+        public void Create_WithParameterFactory_WhenPrefabParameterIsInterface_ReturnsComponentFromClonedPrefab()
+        {
+            var prefabScript = new GameObject("Prefab").AddComponent<ScriptImplementedIInterface>();
+            var result = default(IInterface);
+
+            try
+            {
+                var container = new Container();
+                container.BindFactory<IInterface, IInterface>()
+                    .To<ScriptImplementedIInterface>()
+                    .FromComponentInNewPrefab()
+                    .AsTransient();
+
+                var factory = container.Resolve<Factory<IInterface, IInterface>>();
+                result = factory.Create(prefabScript);
+
+                Assert.That(result, Is.TypeOf<ScriptImplementedIInterface>());
+                Assert.That(result, Is.Not.SameAs(prefabScript));
+                Assert.That(((Component)result).gameObject, Is.Not.SameAs(prefabScript.gameObject));
+            }
+            finally
+            {
+                if (result is Component resultComponent)
+                    UnityEngine.Object.DestroyImmediate(resultComponent.gameObject);
+
+                UnityEngine.Object.DestroyImmediate(prefabScript.gameObject);
+            }
+        }
+
+        [Test]
+        public void BindFactoryWithParameter_WhenParameterTypeIsNotPrefabType_ThrowsArgumentException()
+        {
+            var container = new Container();
+
+            Assert.That(
+                () => container.BindFactory<float, Script>().FromComponentInNewPrefab(),
+                Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void BindFactoryWithParameter_WhenResultTypeIsNotComponentOrInterface_ThrowsArgumentException()
+        {
+            var container = new Container();
+
+            Assert.That(
+                () => container.BindFactory<GameObject, Product>().FromComponentInNewPrefab(),
+                Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
+        public void Create_WithParameterFactory_WhenPrefabIsNull_ThrowsArgumentNullException()
+        {
+            var container = new Container();
+            container.BindFactory<GameObject, Script>().FromComponentInNewPrefab().AsTransient();
+
+            var factory = container.Resolve<Factory<GameObject, Script>>();
+
+            Assert.That(
+                () => factory.Create(null),
+                Throws.TypeOf<ArgumentNullException>());
+        }
+
+        [Test]
+        public void Create_WithParameterFactory_WhenPrefabDoesNotHaveRequestedComponent_ThrowsArgumentException()
+        {
+            var prefab = new GameObject("Prefab");
+
+            try
+            {
+                var container = new Container();
+                container.BindFactory<GameObject, Script>().FromComponentInNewPrefab().AsTransient();
+
+                var factory = container.Resolve<Factory<GameObject, Script>>();
+
+                Assert.That(
+                    () => factory.Create(prefab),
+                    Throws.TypeOf<ArgumentException>());
+            }
+            finally
+            {
+                UnityEngine.Object.DestroyImmediate(prefab);
+            }
+        }
+
+        [Test]
+        public void Create_WithParameterFactory_WhenInterfaceParameterIsNotComponent_ThrowsArgumentException()
+        {
+            var container = new Container();
+            container.BindFactory<IInterface, Script>().FromComponentInNewPrefab().AsTransient();
+
+            var factory = container.Resolve<Factory<IInterface, Script>>();
+
+            Assert.That(
+                () => factory.Create(new ClassImplementedIInterface()),
+                Throws.TypeOf<ArgumentException>());
+        }
+
+        [Test]
         public void BindFactory_WhenFactoryTypeAlreadyBoundByFactoryBinding_ThrowsInvalidOperationException()
         {
             var container = new Container();
